fb5153c68de313ee40b4f85d0bae59dc4e8a47dd
areadox <areadox@gmail.com>
2023-09-16 19:08:22 +0000

    modified:   character_creator/character_creator.py

--- character_creator/character_creator.py
+++ character_creator/character_creator.py
@@ -549,6 +549,8 @@ class CmdWer(MuxAccountCommand):
             % (table, "One" if is_one else naccounts, "" if is_one else "s")
         )
 
+
+
 class CmdEnde(MuxAccountCommand):
     """
     quit the game
